cf73fbf836081277e0cd323f7ed44964988e780a
DevAbhi-Chauhan <abhi099chauhan@gmail.com>
2025-03-19 18:55:30 +0600

    third commit

--- backend/scripts/start_backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/backend
-npm install
-nohup node server.js > backend.log 2>&1 &
-
-chmod +x scripts/start_backend.sh
--- frontend/scripts/restart_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo systemctl restart nginx
-
-chmod +x scripts/restart_server.sh
-
